e3298041c41781f2c6e3b3852f257894f04c7958
nlunscher <nolan_lunscher@hotmail.com>
2017-06-01 18:45:31 -0400

    made progress on NN

--- nl_CartPole_NN.py
+++ nl_CartPole_NN.py
@@ -28,14 +28,18 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 
 class QL_NN:
-    MAX_REPLAY_STEPS = 1000*10
-    RANDOM_ACTION_PROB = 0.1*5
-    MAX_EPISODES = 1000
+    MAX_REPLAY_STEPS = 10000
+    MAX_EPISODES = 300
     MAX_ENV_STEPS = 1000
     QL_GAMMA = 0.9
+    RANDOM_ACTION_PROB = 0.1*3
+    RANDOM_DECAY_RATE = 1.0/MAX_EPISODES
     WATCH_INTERVAL = 10
     SAMPLE_TRAIN_SIZE = 128
     LEARNING_RATE = 0.001
+    FINAL_SCORE_PLACEHOLDER = 9999
+    REMOVE_WORST = False
+    VAL_GAMES = 10
 
     def __init__(self, env_name, sess):
         self.env = gym.make(env_name)
@@ -45,7 +49,7 @@ class QL_NN:
         self.action_size = self.env.action_space.n
         print "Env:", env_name, "Observations:", self.obs_size, "Actions:", self.action_size
 
-        self.replay_memory = [] # [state_obs, action, reward, next_state_obs, is_done]
+        self.replay_memory = [] # [state_obs, action, reward, next_state_obs, is_done, ep_num, final_score]
 
         self.agent_is_trained = False
         self.make_agent()
@@ -99,30 +103,31 @@ class QL_NN:
 
             time.sleep(0.1)
 
-    def agent_play(self):
+    def agent_play(self, show_game = True):
         observation = self.env.reset()
         total_steps = 0
         for t in range(1000):
-            self.env.render()
-            action = self.agent_action(observation)
+            if show_game:
+                self.env.render()
+            action, predicted_Qs = self.agent_action(observation)
             new_observation, reward, done, info = self.env.step(action)
+            # print predicted_Qs
             if done:
                 total_steps = t + 1
-                print("Agent finished after {} timesteps".format(total_steps))
+                if show_game:
+                    print("Agent finished after {} timesteps".format(total_steps))
                 break
 
             observation = new_observation
 
-            time.sleep(0.05)
+            if show_game:
+                time.sleep(0.05)
         return total_steps
 
     def agent_action(self, observation):
-        if self.agent_is_trained or True:
-            predicted_Qs = self.sess.run(self.Qs_nn, feed_dict={self.obs_in:[observation]})
-            action = np.argmax(predicted_Qs)
-        else:
-            action = self.env.action_space.sample()
-        return action
+        predicted_Qs = self.sess.run(self.Qs_nn, feed_dict={self.obs_in:[observation]})
+        action = np.argmax(predicted_Qs)
+        return action, predicted_Qs
 
     def train_agent(self):
         # print "Training Agent"
@@ -130,16 +135,16 @@ class QL_NN:
 
         max_predicted_Qs = np.zeros(self.SAMPLE_TRAIN_SIZE)
         for i in range(self.SAMPLE_TRAIN_SIZE):
-            qs = self.agent_action(batch_memory[i][0])
+            act, qs = self.agent_action(batch_memory[i][3])
             max_predicted_Qs[i] = np.max(qs)
 
         max_target_Q = np.zeros(self.SAMPLE_TRAIN_SIZE)
         target_Qs = np.zeros((self.SAMPLE_TRAIN_SIZE, self.action_size))
         target_actions = np.zeros(self.SAMPLE_TRAIN_SIZE)
         for i in range(self.SAMPLE_TRAIN_SIZE):
-            state_obs, action, reward, next_state_obs, is_done = batch_memory[i]
+            state_obs, action, reward, next_state_obs, is_done, ep_num, final_score = batch_memory[i]
 
-            max_target_Q[i] = reward 
+            max_target_Q[i] = reward
             if not is_done:
                 max_target_Q[i] += self.QL_GAMMA * max_predicted_Qs[i]
             target_Qs[i][action] = max_target_Q[i]
@@ -168,41 +173,74 @@ class QL_NN:
 
         agent_steps = []
         overall_steps = 0
+        last_loss = -1
+        start_time = time.time()
         for ep in range(self.MAX_EPISODES):
             if ep % self.WATCH_INTERVAL == 0:
                 # watch the agent play
+                print ep, time.time() - start_time
+                total_avg = 0
+                for i in range(self.VAL_GAMES):
+                    total_avg += self.agent_play(show_game=False)
+                total_avg /= 1.0 * self.VAL_GAMES
+                agent_steps.append(total_avg)
+                print "Average", total_avg
                 total_steps = self.agent_play()
-                agent_steps.append(total_steps)
 
             obs = self.env.reset()
+            total_reward = 0
             for step in range(self.MAX_ENV_STEPS):
                 # self.env.render()
 
-                if random.random() < self.RANDOM_ACTION_PROB:
-                    action = self.env.action_space.sample()
+                decayed_random_prob = self.RANDOM_ACTION_PROB * np.exp(-1*self.RANDOM_DECAY_RATE*ep)
+                if random.random() < decayed_random_prob:
+                    action = self.env.action_space.sample() # reandom action
                 else:
-                    action = self.agent_action(obs)
+                    action, predicted_Qs = self.agent_action(obs)
 
                 new_obs, reward, is_done, info = self.env.step(action)
-
                 if is_done:
                     reward = -100
+                total_reward += reward
 
-                self.replay_memory.append([obs, action, reward, new_obs, is_done])
+                self.replay_memory.append([obs, action, reward, new_obs, is_done, ep, self.FINAL_SCORE_PLACEHOLDER])
                 if len(self.replay_memory) > self.MAX_REPLAY_STEPS:
-                    self.replay_memory.pop(0)
+                    if self.REMOVE_WORST:
+                        lowest_final_index = 0
+                        min_final = self.FINAL_SCORE_PLACEHOLDER
+                        max_final = -self.FINAL_SCORE_PLACEHOLDER
+                        for i in range(len(self.replay_memory)):
+                            if self.replay_memory[i][-1] < min_final:
+                                min_final = self.replay_memory[i][-1]
+                                lowest_final_index = i
+                            if self.replay_memory[i][-1] > max_final and self.replay_memory[i][-1] != self.FINAL_SCORE_PLACEHOLDER:
+                                max_final = self.replay_memory[i][-1]
+                        popped = self.replay_memory.pop(lowest_final_index)
+                    else:
+                        self.replay_memory.pop(0)
 
-                if is_done:
+                    if False:
+                        print "Ep", ep, "step", step, "Popped", popped[-3:], "Max", max_final, \
+                                "Loss", last_loss, "Test", agent_steps[-1], \
+                                "Rand_prob", decayed_random_prob
+
+                if is_done or step == self.MAX_ENV_STEPS - 1:
                     # print("Episode finished after {} timesteps".format(step+1))
+                    # replace the placeholders with real totals
+                    # self.replay_memory[self.replay_memory[:,5] == self.FINAL_SCORE_PLACEHOLDER,5] = total_reward
+                    for i in range(len(self.replay_memory)):
+                         if self.replay_memory[i][-1] == self.FINAL_SCORE_PLACEHOLDER:
+                            self.replay_memory[i][-1] = total_reward
                     break
 
                 obs = new_obs
                 # time.sleep(0.1)
 
-                if len(self.replay_memory) > self.SAMPLE_TRAIN_SIZE:
+                if len(self.replay_memory) >= self.SAMPLE_TRAIN_SIZE:
                     loss, Qs_nn, target_Qs, Q_masked = self.train_agent()
-                    if overall_steps % 100 == 0:
-                        print "Episode", ep, "Total Steps", overall_steps, "Loss", loss
+                    last_loss = loss
+                    # if overall_steps % 100 == 0:
+                    #     print "Episode", ep, "Total Steps", overall_steps, "Total Reward", total_reward, "Loss", loss
                         # for i in range(2):
                         #     print Qs_nn[i], Q_masked[i], target_Qs[i]
 
--- nl_CartPole_Tree.py
+++ nl_CartPole_Tree.py
@@ -27,13 +27,16 @@ import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeRegressor
 
 class QL_Tree:
-    MAX_REPLAY_STEPS = 128*3
-    RANDOM_ACTION_PROB = 0.1
-    MAX_EPISODES = 1000
+    MAX_REPLAY_STEPS = 20000
+    RANDOM_ACTION_PROB = 0.1*3
+    RANDOM_DECAY_RATE = 1.0/MAX_REPLAY_STEPS
+    MAX_EPISODES = 1000*5
     MAX_ENV_STEPS = 1000
-    QL_GAMMA = 0.8
+    QL_GAMMA = 0.9
     WATCH_INTERVAL = 20
-    SAMPLE_TRAIN_SIZE = 128
+    SAMPLE_TRAIN_SIZE = 10000
+    FINAL_SCORE_PLACEHOLDER = 9999
+    REMOVE_WORST = True
 
     def __init__(self, env_name):
         self.env = gym.make(env_name)
@@ -42,9 +45,11 @@ class QL_Tree:
         self.action_size = self.env.action_space.n
         print "Env:", env_name, "Observations:", self.obs_size, "Actions:", self.action_size
 
-        self.replay_memory = [] # [state_obs, action, reward, next_state_obs, is_done]
+        self.replay_memory = [] # [state_obs, action, reward, next_state_obs, is_done, ep_num, final_score]
 
-        self.agent = DecisionTreeRegressor()
+        self.agent = []
+        for i in range(self.action_size):
+            self.agent.append(DecisionTreeRegressor())
         self.agent_is_trained = False
 
     def random_play(self):
@@ -63,93 +68,139 @@ class QL_Tree:
 
             time.sleep(0.1)
 
-    def agent_play(self):
+    def agent_play(self, show_game = True):
         observation = self.env.reset()
         total_steps = 0
         for t in range(1000):
-            self.env.render()
-            action = self.agent_action(observation)
+            if show_game:
+                self.env.render()
+            action, predicted_Qs = self.agent_action(observation)
             new_observation, reward, done, info = self.env.step(action)
+            print predicted_Qs
             if done:
                 total_steps = t + 1
-                print("Agent finished after {} timesteps".format(total_steps))
+                if show_game:
+                    print("Agent finished after {} timesteps".format(total_steps))
                 break
 
             observation = new_observation
 
-            time.sleep(0.05)
+            if show_game:
+                time.sleep(0.05)
         return total_steps
 
     def agent_action(self, observation):
         if self.agent_is_trained:
-            predicted_Qs = self.agent.predict([observation])
+            predicted_Qs = []
+            for i in range(self.action_size):
+                predicted_Qs.append(self.agent[i].predict([observation]))
+            # print predicted_Qs
             action = np.argmax(predicted_Qs)
         else:
+            predicted_Qs = np.zeros(self.action_size)
             action = self.env.action_space.sample()
-        return action
+        return action, predicted_Qs
 
     def train_agent(self):
         # print "Training Agent"
-        batch_memory = random.sample(self.replay_memory, self.SAMPLE_TRAIN_SIZE)
+        batch_memory = random.sample(self.replay_memory, min(len(self.replay_memory), self.SAMPLE_TRAIN_SIZE))
+        batch_size = len(batch_memory)
 
-        max_predicted_Qs = np.zeros(self.SAMPLE_TRAIN_SIZE)
-        for i in range(self.SAMPLE_TRAIN_SIZE):
-            qs = self.agent_action(batch_memory[i][0])
+        max_predicted_Qs = np.zeros(batch_size)
+        for i in range(batch_size):
+            action, qs = self.agent_action(batch_memory[i][3])
             max_predicted_Qs[i] = np.max(qs)
 
-        max_target_Q = np.zeros(self.SAMPLE_TRAIN_SIZE)
-        target_Qs = np.zeros((self.SAMPLE_TRAIN_SIZE, self.action_size))
-        for i in range(self.SAMPLE_TRAIN_SIZE):
-            state_obs, action, reward, next_state_obs, is_done = batch_memory[i]
+        max_target_Q = np.zeros(batch_size)
+        target_Qs = []
+        features = []
+        for i in range(self.action_size):
+            features.append([])
+            target_Qs.append([])
+        for i in range(batch_size):
+            state_obs, action, reward, next_state_obs, is_done, ep_num, final_score = batch_memory[i]
 
             max_target_Q[i] = reward 
             if not is_done:
                 max_target_Q[i] += self.QL_GAMMA * max_predicted_Qs[i]
-            target_Qs[i][action] = max_target_Q[i]
+            target_Qs[action].append(max_target_Q[i])
+            features[action].append(state_obs)
 
-        features = [f[0] for f in batch_memory]
+        # features = [f[0] for f in batch_memory]
 
-        self.agent.fit(features, target_Qs)
-        # print "Agent Score", self.agent.score(features, target_Qs)
+        # print "showing"
+        for i in range(self.action_size):
+            self.agent[i].fit(features[i], target_Qs[i])
+            # print "Agent Score", self.agent[i].score(features[i], target_Qs[i])
+
         # # print features[0]
-        # print target_Qs[:10]
-        # print self.agent.predict(features)[:10]
-        self.agent_is_trained = True
+            # print "-targ", target_Qs[i][:5]
+            # print "pred", self.agent[i].predict(features[i])[:5]
+
+        if not self.agent_is_trained:
+            self.agent_is_trained = True
+            print "Trained"
 
     def train(self):
         agent_steps = []
         for ep in range(self.MAX_EPISODES):
             if ep % self.WATCH_INTERVAL == 0:
                 # watch the agent play
+                total_avg = 0
+                for i in range(10):
+                    total_avg += self.agent_play(show_game=False)
+                total_avg /= 10.0
+                agent_steps.append(total_avg)
+                print "Average", total_avg
                 total_steps = self.agent_play()
-                agent_steps.append(total_steps)
 
             obs = self.env.reset()
+            total_reward = 0
             for step in range(self.MAX_ENV_STEPS):
                 # self.env.render()
 
-                if random.random() < self.RANDOM_ACTION_PROB:
+                decayed_random_prob = self.RANDOM_ACTION_PROB * np.exp(-1*self.RANDOM_DECAY_RATE*ep)
+                if random.random() < decayed_random_prob:
                     action = self.env.action_space.sample()
                 else:
-                    action = self.agent_action(obs)
+                    action,_ = self.agent_action(obs)
 
                 new_obs, reward, is_done, info = self.env.step(action)
 
                 if is_done:
                     reward = -100
+                total_reward += reward
 
-                self.replay_memory.append([obs, action, reward, new_obs, is_done])
+                self.replay_memory.append([obs, action, reward, new_obs, is_done, ep, self.FINAL_SCORE_PLACEHOLDER])
                 if len(self.replay_memory) > self.MAX_REPLAY_STEPS:
-                    self.replay_memory.pop(0)
+                    if self.REMOVE_WORST:
+                        lowest_final_index = 0
+                        min_final = self.FINAL_SCORE_PLACEHOLDER
+                        max_final = -self.FINAL_SCORE_PLACEHOLDER
+                        for i in range(len(self.replay_memory)):
+                            if self.replay_memory[i][-1] < min_final:
+                                min_final = self.replay_memory[i][-1]
+                                lowest_final_index = i
+                            if self.replay_memory[i][-1] > max_final and self.replay_memory[i][-1] != self.FINAL_SCORE_PLACEHOLDER:
+                                max_final = self.replay_memory[i][-1]
+                        popped = self.replay_memory.pop(lowest_final_index)
+                    else:
+                        self.replay_memory.pop(0)
 
-                if is_done:
-                    # print("Episode finished after {} timesteps".format(step+1))
+                    if ep % 10 == 0 and False:
+                        print "Ep", ep, "step", step, "Popped", popped[-3:], "Max", max_final, \
+                            "Test", agent_steps[-1]
+
+                if is_done or step == self.MAX_ENV_STEPS - 1:
+                    for i in range(len(self.replay_memory)):
+                         if self.replay_memory[i][-1] == self.FINAL_SCORE_PLACEHOLDER:
+                            self.replay_memory[i][-1] = total_reward
                     break
 
                 obs = new_obs
                 # time.sleep(0.1)
 
-            if len(self.replay_memory) > self.SAMPLE_TRAIN_SIZE:
+            if len(self.replay_memory) >= 100:
                 self.train_agent()
 
         plt.plot(range(self.MAX_EPISODES/self.WATCH_INTERVAL), agent_steps)
